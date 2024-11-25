67fa696ba715483dbebf7f353be2b5dd1a3ab9ac
nnamdi nwaocha <nnamdinwaocha@gmail.com>
2024-11-25 16:59:56 +0100

    feat(): header

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./sections/Header";
+function App() {
+  return ( 
+    <main className="overflow-hidden">
+      <Header></Header>
+    </main>
+   );
+}
+
+export default App;
